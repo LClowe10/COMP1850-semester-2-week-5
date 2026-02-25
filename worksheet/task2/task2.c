acd05e671be00ce4283e61c116b4db850ca5cb56
Lucas Parbott Clowe <cmnp0658@leeds.ac.uk>
2026-02-25 10:44:32 +0000

    More work on worksheet

--- worksheet/task2/task2.c
+++ worksheet/task2/task2.c
@@ -2,8 +2,8 @@
  * Sem 2, week 5
  *
  * Portfolio submission: Hexadecimal to Decimal Converter
- * Name:
- * ID:
+ * Name: Lucas Parbott Clowe
+ * ID: 201972343
  */
 
 #include <stdio.h>
@@ -14,12 +14,24 @@ int main(void)
 {
 	long decimal = 0;
 	char hex[9];
+	char buffer[100];
+	int check;
 
+	// Getting the value from the user
 	printf("Enter a hexadecimal: ");
-	fgets(hex, 9, stdin);
+	fgets(buffer, 100, stdin);
+	check = sscanf(buffer, "%c", hex);
+	printf("%s\n", hex);
+	// Validation for the amount of arguments passed
+	if (check - 1) 
+	{
+		printf("Invalid Input\n");
+		return 1;
+	}
 
-	int length = strlen(hex);
-	if (length > 9 || length < 0)
+	// Finding and validating the length of the input
+	int length = (strlen(buffer) - 1);
+	if (length > 8 || length < 1)
 	{
 		printf("Invalid Length\n");
 		return 1;
@@ -27,19 +39,24 @@ int main(void)
 
 	for (int count = 0; count < length; count++)
 	{
+		printf("%d\n", hex[0]);
+		printf("%ld\n", decimal);
 		if ((hex[count] >= 48) && (hex[count] <= 57))
 		{
 			decimal += (hex[count] - 48) * pow(16, length - (count + 1));
+			continue;
 		}
 
 		else if ((hex[count] >= 65) && (hex[count] <= 70))
 		{
 			decimal += (hex[count] - 55) * pow(16, length - (count + 1));
+			continue;
 		}
 
 		else if ((hex[count] >= 97) && (hex[count] <= 102))
 		{
 			decimal += (hex[count] - 87) * pow(16, length - (count + 1));
+			continue;
 		}
 
 		else
